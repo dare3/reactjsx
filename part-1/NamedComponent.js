d9d2cac13c4cda3eb0d7d47d6a85058db4494db6
Dare3 <enitan_segun@yahoo.co.uk>
2024-09-24 20:26:05 -0400

    modified

--- part-1/NamedComponent.js
+++ part-1/NamedComponent.js
@@ -1,4 +1,8 @@
 // NamedFunction component that accept a prop of name & display the name
 function NamedFunction ({name}) {
-    return <p>My name is {name}.</p>;
+    return(
+    <p>My name is {name}.</p>
+
+    ); 
 }
+export default NamedFunction();
